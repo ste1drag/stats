eccc5a1fb355afafab0c23603247348b97cf4207
Stevan Dragović <stevandragovic.10@gmail.com>
2023-03-28 13:27:41 +0200

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const MatchReader_1 = require("./MatchReader");
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_enum_1 = require("./MatchResult.enum");
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+let manUnitedWins = 0;
+for (let match of matchReader.matches) {
+    if ((match[1] === 'Man United' && match[5] === MatchResult_enum_1.MatchResult.HomeWin) || (match[2] === 'Man United' && match[5] === MatchResult_enum_1.MatchResult.AwayWin)) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
